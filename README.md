b64a583458ae1b92a8e72ffe33004b8643780b71
programmingAtGoose <joost.tanski@gmail.com>
2025-08-02 20:31:57 +0200

    v0.1 done

---
+++ README.md
@@ -0,0 +1,35 @@
+# Dice Roller
+
+Roll dice
+
+## Description
+
+a simple project to simulate dice rolls from dice notation
+## Getting Started
+
+### Dependencies
+
+* Java 8
+* Javac
+
+### Installing
+
+* Download the GitHub repo
+
+### Executing program
+
+* open cmd
+* navigate to the directory of the install
+* run with java
+```
+{yourInstallPath}\diceRoller\src> java diceRoller.java
+```
+
+## Author
+
+programmingAtGoose (https://GitHub.com/programmingAtGoose)
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ diceRoller/src/diceRoller.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class diceRoller {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.print("Dice notation to roll: ");
+        String diceRollString = sc.nextLine();
+
+        if(diceRollString.indexOf("d") == -1){
+            sc.close();
+            throw new IllegalArgumentException("no \"d\" delimitor found");
+        }
+
+        int dieDelimiterIndex = diceRollString.indexOf("d");
+
+        int dieAmt = Integer.parseInt(diceRollString.substring(0, dieDelimiterIndex));
+        System.out.println("Die amt: " + dieAmt);
+
+        int dieSize = Integer.parseInt(diceRollString.substring(dieDelimiterIndex + 1,diceRollString.length()));
+        System.out.println("Die size: " + dieSize);
+
+        int total = 0;
+        int roll;
+        System.out.print("Rolls: [");
+        for (int i = 0; i < dieAmt; i++) {
+            roll = rand.nextInt(dieSize + 1);
+            total += roll;
+            System.out.print(roll + " ");
+        }
+
+        System.out.print("]\n");
+        System.out.println("Total: " + total);
+
+        sc.close();
+    }
+}
